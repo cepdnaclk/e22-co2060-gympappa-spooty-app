3a7480a12b2398a81e2f3a2213c20f661773a4bf
Ranushi Jayalath <e22157@eng.pdn.ac.lk>
2026-04-19 23:23:51 +0530

    Connected frontend to backend and added equipment list functionality

--- code/equipment-handling/backend/index.js
+++ code/equipment-handling/backend/index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import equipmentRoutes from "./routes/equipmentRoutes.js";
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
